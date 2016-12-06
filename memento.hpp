bc649690695c76af0fcaf39def252bf535bfdf8d
Varvar <Sergeybushuev14@gmail.com>
2016-12-06 23:40:33 +0300

    Update memento.hpp

--- memento.hpp
+++ memento.hpp
@@ -1,5 +1,42 @@
 #ifndef _MEMENTO_HPP_
-#define _MEMENTO_HPP_
-class 
+#define _MEMENTO_HPP_ 
 
+template <typename T>
+class Memento
+{
+  frined class Originator;
+  private:
+     Memento(const T& state);
+     ~Memento();
+     void SetState(const T& state);
+     T GetState();
+     T _state;
+}
+
+template <typename T>
+class Originator
+{
+  public:
+     Originator();
+     Originator(const T& state);
+     ~Originator();
+     void RestoreToMemento(Memento* pMemento);
+     Memento* CreateMemento();
+     void SetState(const T& state);
+     T GetState();
+     void show();
+  private:
+     T _state;
+};
+
+class Caretaker
+{
+  public:
+     Caretaker();
+     ~Caretaker();
+     void SetMemento(Memento*);
+     Memento* GetMemento();
+  private:
+     Memento* _memento;
+};
 #endif
